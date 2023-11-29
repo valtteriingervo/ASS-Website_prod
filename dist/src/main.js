48cba62a204ec7a61e6b61ff34f68b5229357845
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-11-29 08:52:26 +0200

    add the process.env.PORT

--- dist/src/main.js
+++ dist/src/main.js
@@ -9,7 +9,7 @@ async function bootstrap() {
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
     }));
-    await app.listen(3333);
+    await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
